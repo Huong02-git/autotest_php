6759be83ca70cade3baab54ee547265b94482358
Huong02-git <vuongthuyhuong6@gmail.com>
2023-08-08 14:18:03 +0700

    bài tập

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+echo "hiiiiiiiiiiiiii"
+function arrange($array){
+    $numbers = array(4, 6, 2, 22, 11);
+    rsort($numbers);
+    foreach( $numbers as $n) {
+       return "$n <br>";
+}
+}
+?>
